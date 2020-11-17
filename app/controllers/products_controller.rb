bcc1ed79847af2791ff8cafcf729f19c88a51162
rordev456 <devidev45@proton.me>
2020-11-17 10:25:52 +0700

    list products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+  def index
+    @per_page = Settings.paging.p_8
+    @products = Product.page(params[:page]).per(@per_page)
+  end
+
+  def show
+    @product = Product.find_by id: params[:id]
+    return if @product
+
+    flash[:danger] = t "controllers.products_controller.product_not_found"
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    root "static_pages#home"
+    root "products#index"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
-    get "/search" => "pages#search", :as => "search_page"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    get "/search" => "pages#search", as: "search_page"
+    resources :brands, except: [:destroy, :show]
+    resources :products, only: [:index, :show]
     namespace :admin do
       get "/home", to: "static_pages#home"
       resources :brands, except: [:destroy, :show]
